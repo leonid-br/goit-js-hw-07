d051b0542c962b102e43de630bbee3f64dd7f31c
unknown <74738757+leonid-br@users.noreply.github.com>
2021-05-12 22:56:24 +0300

    Задача 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const selectedById = document.querySelector('#categories');
+console.log(`В списке ${selectedById.children.length} категории`);
+
+const menuItem = document.querySelectorAll('.item');
+function findItem() {
+    menuItem.forEach(elem =>
+        console.log(`${elem.children[0].innerHTML}: ${elem.children[1].children.length}`)
+    )}
+findItem()
