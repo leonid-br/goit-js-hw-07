a05dead3cc11ff3b219500d958a9e5a80c48d958
unknown <74738757+leonid-br@users.noreply.github.com>
2021-05-13 01:05:03 +0300

    Задача 2

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,6 @@ console.log(`В списке ${selectedById.children.length} категории`
 const menuItem = document.querySelectorAll('.item');
 function findItem() {
     menuItem.forEach(elem =>
-        console.log(`${elem.children[0].innerHTML}: ${elem.children[1].children.length}`)
+        console.log(`${elem.children[0].textContent}: ${elem.children[1].children.length}`)
     )}
 findItem()
