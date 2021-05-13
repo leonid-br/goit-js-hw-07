78e1a06e9f85dcac21093e23dca245463068b51f
unknown <74738757+leonid-br@users.noreply.github.com>
2021-05-13 03:00:03 +0300

    Задача 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,12 +6,17 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
 const listEl = document.querySelector('#ingredients')
 
-const getArrItems = ingredients.map(elem => {
-        const item = document.createElement('li')
-        item.textContent = elem
-        return item
+const getArrItems = elements => {
+    return elements.map(element => {
+        const itemEl = document.createElement('li')
+        itemEl.textContent = element
+        return itemEl
     })
+}
 
-listEl.append(...getArrItems)
+const arrayItems = getArrItems(ingredients)
+
+listEl.append(...arrayItems)
