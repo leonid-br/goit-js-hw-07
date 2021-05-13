ca33c08cf1dd9eab70a9cad9738ce6cd61ba25f8
unknown <74738757+leonid-br@users.noreply.github.com>
2021-05-14 01:35:51 +0300

    Задача 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,18 @@
 const input = document.querySelector('#validation-input')
 
+input.addEventListener('focus', onInputFocus);
 input.addEventListener('blur', onInputBlur);
 
-function onInputBlur(ev) {
-
-    if (ev.currentTarget.value.length !== Number(ev.currentTarget.dataset.length)) {
-        input.classList.add('.invalid')
-    } else {
-        document.input.classList.add('.valid')
-    }
+function onInputFocus(ev) {
+    input.classList.remove('invalid')
+    input.classList.remove('valid')
+    
 }
+
+function onInputBlur(ev) {
+    ev.currentTarget.value.length !== Number(ev.currentTarget.dataset.length) ?
+    input.classList.add('invalid'):
+    input.classList.add('valid')
+}
+
 
