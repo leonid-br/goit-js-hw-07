d35d3e29cc336b13354627982e103fc83cba0106
unknown <74738757+leonid-br@users.noreply.github.com>
2021-05-16 12:53:55 +0300

    Правки задача 4

--- js/task-04.js
+++ js/task-04.js
@@ -2,16 +2,30 @@ const counterValue = document.querySelector('#value')
 const incrementBtn = document.querySelector('button[data-action="increment"]')
 const decrementBtn = document.querySelector('button[data-action="decrement"]')
 
-incrementBtn.addEventListener('click', onIncrementCount)
-decrementBtn.addEventListener('click', onDecrementCount)
+// incrementBtn.addEventListener('click', onIncrementCount)
+// decrementBtn.addEventListener('click', onDecrementCount)
 
 
-function onIncrementCount (ev) {
-    let count = Number(counterValue.innerHTML)
-    counterValue.innerHTML = count+=1
+// function onIncrementCount (ev) {
+//     let count = Number(counterValue.innerHTML)
+//     counterValue.innerHTML = count+=1
+// }
+
+// function onDecrementCount (ev) {
+//     let count = Number(counterValue.innerHTML)
+//     counterValue.innerHTML = count-=1
+// }
+
+function countClick(el) {
+    let counterValue = 0;
+    incrementBtn.addEventListener('click', () => {
+        counterValue += 1;
+        el.textContent = counterValue;
+    });
+    decrementBtn.addEventListener('click', () => {
+        counterValue -= 1;
+        el.textContent = counterValue;
+    });
 }
 
-function onDecrementCount (ev) {
-    let count = Number(counterValue.innerHTML)
-    counterValue.innerHTML = count-=1
-}
+countClick(counterValue)
